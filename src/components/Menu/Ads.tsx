4afda6763c73ae4cfc50d38c10c1c906ae198a00
hochonin93 <ho.chon.in93@gmail.com>
2023-06-29 17:41:36 +0800

    小於768隱藏廣告，更改title

--- src/components/Menu/Ads.tsx
+++ src/components/Menu/Ads.tsx
@@ -4,8 +4,23 @@ import useStore from '@store/store';
 
 
 const Ads = () => {
+    const hideAds = useStore((state) => state.hideAds);
+    const setHideAds = useStore((state) => state.setHideAds);
+    const windowWidthRef = useRef<number>(window.innerWidth);
+
+    useEffect(() => {
+        if (window.innerWidth < 768) setHideAds(true);
+        window.addEventListener('resize', () => {
+            if (
+                windowWidthRef.current !== window.innerWidth &&
+                window.innerWidth < 768
+            )
+                setHideAds(true);
+        });
+    }, []);
+
     return (
-        <div className={`group/menu dark bg-gray-900 fixed md:inset-y-0 md:flex md:w-[260px] md:flex-col transition-transform z-[999] top-0 right-0 h-full max-md:w-3/4`}>
+        <div className={`group/menu dark bg-gray-900 fixed md:inset-y-0 md:flex md:w-[260px] md:flex-col transition-transform z-[999] top-0 right-0 h-full max-md:w-3/4 ${hideAds ? 'hidden' : ''}`}>
             <Adsense
                 client="ca-pub-9272842344184040"
                 slot="8709450567"
