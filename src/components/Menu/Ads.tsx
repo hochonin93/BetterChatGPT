b392804a6c0ccabe27e4487600f3ebcb52554811
hochonin93 <ho.chon.in93@gmail.com>
2023-06-29 13:57:08 +0800

    fix bug

--- src/components/Menu/Ads.tsx
+++ src/components/Menu/Ads.tsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useRef } from 'react';
-
+import { Adsense } from '@ctrl/react-adsense';
 import useStore from '@store/store';
-import AdsComponent from './AdsComponent';
+
 
 const Ads = () => {
     return (
         <div className={`group/menu dark bg-gray-900 fixed md:inset-y-0 md:flex md:w-[260px] md:flex-col transition-transform z-[999] top-0 right-0 h-full max-md:w-3/4`}>
             fregerg
-            <AdsComponent dataAdSlot='5960307417' />
+            <Adsense
+                client="ca-pub-9272842344184040"
+                slot="5960307417"
+            />
         </div>
     )
 }
