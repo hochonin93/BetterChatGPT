6c4edf8e133c3fd0d20b2daa5b0037028ebd5261
hochonin93 <ho.chon.in93@gmail.com>
2023-07-10 13:21:05 +0800

    新增next-seo

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import Head from 'next/head';
 import Script from 'next/script';
+import { NextSeo } from 'next-seo';
 
 const IndexPage = () => {
   return (
     <div>
-      <div>
+      <Head>
+        <NextSeo
+          title='免費ChatGPT工具'
+          description='免費ChatGPT工具'
+          openGraph={{
+            title: '免費ChatGPT工具',
+            description: '免費ChatGPT工具',
+            images: [
+              {
+                url: '/pexels-andrew-neel-15863044.jpg',
+              },
+            ],
+          }}
+          link={[
+            {
+              rel: 'icon',
+              href: '/favicon-32x32.png',
+            },
+          ]}
+        />
         <meta charSet='UTF-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <link
@@ -25,28 +45,19 @@ const IndexPage = () => {
           sizes='16x16'
           href='favicon-16x16.png'
         />
-        <meta property='og:image' content='./pexels-andrew-neel-15863044.jpg' />
-        <meta
-          name='twitter:image'
-          content='./pexels-andrew-neel-15863044.jpg'
-        />
-        <meta name='description' content='免費ChatGPT工具' />
-        <meta name='twitter:description' content='免費ChatGPT工具' />
-        <meta name='twitter:title' content='免費ChatGPT工具' />
-        <meta name='twitter:card' content='summary_large_image' />
-        <title>免費ChatGPT工具</title>
-        <Script
-          type='module'
-          crossOrigin='anonymous'
-          src='./dist/assets/index-c19a1a20.js'
-        />
-        <link rel='stylesheet' href='./dist/assets/index-0c0990c7.css' />
-        <Script
-          async
-          src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9272842344184040'
-          crossOrigin='anonymous'
-        />
-      </div>
+      </Head>
+      <Script
+        type='module'
+        crossOrigin='anonymous'
+        src='./dist/assets/index-c19a1a20.js'
+      />
+
+      <Script
+        async
+        src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9272842344184040'
+        crossOrigin='anonymous'
+      />
+      <link rel='stylesheet' href='./dist/assets/index-0c0990c7.css' />
       <div>
         <div id='root'></div>
         <div id='modal-root'></div>
